d26a9112a799dbeb3385816950ff76117067fa76
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-11-20 19:06:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+ function updateTimer() {
+      var timerElement = document.getElementById('timer');
+      var currentDateTime = new Date();
+      
+      // Format the date and time
+      var dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+      var timeOptions = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+      var formattedDate = currentDateTime.toLocaleDateString('en-US', dateOptions);
+      var formattedTime = currentDateTime.toLocaleTimeString('en-US', timeOptions);
+      
+      // Display the formatted date and time
+      timerElement.textContent = formattedDate + ' ' + formattedTime;
+    }
+
+    // Update the timer every second
+    setInterval(updateTimer, 1000);
+
+    // Initial update
+    updateTimer();
